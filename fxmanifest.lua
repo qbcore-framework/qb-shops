9d35de3eb5632e15a62edf249a6a04de9579f45a
GhzGarage <gigahertzgarage@mail.com>
2021-07-01 21:48:57 -0500

    GetObject

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,15 +4,13 @@ game 'gta5'
 description 'QB-Shops'
 version '1.0.0'
 
-client_scripts {
-	"client/main.lua",
-	"config.lua",
+shared_scripts { 
+	'@qb-core/import.lua',
+	'config.lua'
 }
 
-server_scripts {
-	"server/main.lua",
-	"config.lua",
-}
+client_script 'client/main.lua'
+server_script 'server/main.lua'
 
 dependencies {
 	'qb-inventory'
