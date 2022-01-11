1d94a990900be9f7cd07bfb2dbf67a6a6594f2d1
unawarepuma <97047857+unawarepuma@users.noreply.github.com>
2022-01-11 21:00:11 +0000

    it translation

---
+++ locale/it.lua
@@ -0,0 +1,14 @@
+local Translations = {
+    info = {
+        interact = "~g~E~w~ - Negozio"
+    },
+    error = {
+        dealer_decline = "Il commerciante rifiuta di mostrarti le armi da fuoco",
+        talk_cop = "Parla con le forze dell'ordine per ottenere una licenza per armi da fuoco"
+    },
+    success = {
+        dealer_verify = "Il commerciante ha verificato la tua licenza d'armi"
+    },
+}
+
+Lang = Locale:new({phrases = Translations})
