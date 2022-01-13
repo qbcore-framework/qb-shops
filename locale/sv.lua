d376ecd620c0786ce4ea4bce5cc9740cf1d2531f
ayatollah <tobias.bovang@supergott.com>
2022-01-13 21:28:13 +0100

    Create sv.lua

---
+++ locale/sv.lua
@@ -0,0 +1,14 @@
+local Translations = {
+    info = {
+        interact = "~g~E~w~ - Affär"
+    },
+    error = {
+        dealer_decline = "Handlaren har nekat att visa dig sina vapen",
+        talk_cop = "Prata med Polisen för att få en vapenlicens"
+    },
+    success = {
+        dealer_verify = "Handlaren har godkänt din vapenlicens"
+    },
+}
+
+Lang = Locale:new({phrases = Translations})
