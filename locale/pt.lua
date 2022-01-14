831cb1220b224aa832a5922f2bd78e7d74cf5125
KUMApt <36710278+DanielCoelh0@users.noreply.github.com>
2022-01-14 00:17:57 +0000

    Create pt.lua

---
+++ locale/pt.lua
@@ -0,0 +1,14 @@
+local Translations = {
+    info = {
+        interact = "~g~E~w~ - Abrir Loja"
+    },
+    error = {
+        dealer_decline = "O Vendedor recusou mostrar o armeiro",
+        talk_cop = "Fala com um agente para obteres o porte de arma"
+    },
+    success = {
+        dealer_verify = "O vendedor está a verificar a tua licença"
+    },
+}
+
+Lang = Locale:new({phrases = Translations})
