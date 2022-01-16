dd2dcb96ea46faab9b0e7564b878fb7c4faf06f6
Master Mind <48193931+iboss21@users.noreply.github.com>
2022-01-16 09:46:09 -0500

    Georgian Language for Shop
    Created Georgia Locale

---
+++ locale/ge.lua
@@ -0,0 +1,14 @@
+local Translations = {
+    info = {
+        interact = "~g~E~w~ - მაღაზია"
+    },
+    error = {
+        dealer_decline = "დილერს არ სურს გაჩვენოთ პროდუქცია ლიცენზიის გარეშე",
+        talk_cop = "მიმართეთ ძალოვან სტრუქტურას ლიცენზიის ასაღებად."
+    },
+    success = {
+        dealer_verify = "დილერმა დაადასტურა ლიცენზიის ვარგისიანობა"
+    },
+}
+
+Lang = Locale:new({phrases = Translations})
