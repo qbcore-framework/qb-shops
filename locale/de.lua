0e9d6e4de6211c36b4aa5061e82becac935030a2
aj <92224515+aj-devs@users.noreply.github.com>
2022-01-19 22:05:10 +0100

    Create de.lua

---
+++ locale/de.lua
@@ -0,0 +1,14 @@
+local Translations = {
+    info = {
+        interact = "~g~E~w~ - Shop"
+    },
+    error = {
+        dealer_decline = "Der Verkäufer möchte dir keine Schusswaffen zeigen",
+        talk_cop = "Rede mit der Polizei um einen Waffenschein zu bekommen"
+    },
+    success = {
+        dealer_verify = "Der verkäufer verifiziert deine Lizenz"
+    },
+}
+
+Lang = Locale:new({phrases = Translations})
