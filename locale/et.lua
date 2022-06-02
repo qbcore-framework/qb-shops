cd5b0ad94f7925b17d4c9822d5f5f17ffac1d97a
Dolbajoba <83533764+Getpwnedbydolbajoba@users.noreply.github.com>
2022-06-02 16:08:58 +0300

    Create et.lua

---
+++ locale/et.lua
@@ -0,0 +1,15 @@
+local Translations = {
+    info = {
+        open_shop = "[E] Pood",
+        sell_chips = "[E] Müü chipid maha"
+    },
+    error = {
+        dealer_decline = "Relvadiiler keeldub sulle relvade näitamisest.",
+        talk_cop = "Räägi riigiametnikuga, et endale relvaluba taodelda."
+    },
+    success = {
+        dealer_verify = "Diiler kinnitas su relvaloa."
+    },
+}
+
+Lang = Locale:new({phrases = Translations})
